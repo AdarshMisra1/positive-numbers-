fedcc509c7574e351c65b71a0fd2c0989d3ea067
AdarshMisra1 <88109868+AdarshMisra1@users.noreply.github.com>
2021-07-29 01:44:21 +0530

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,10 @@
+list1 = [12, -7, 5, 64, -14] 
+list2 = [12, 14, -95, 3]
+
+output1= list(filter(lambda x: x>0, list1))
+    
+print("output",output1)
+
+output2= list(filter(lambda x: x>0, list2))
+    
+print("output",output2)
